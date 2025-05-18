267ab61746f33e2eec705cfa87f6288c2f7aff4b
Stu Bolderson <stu.bolderson@aol.com>
2025-05-18 15:41:04 +0200

    documentation(server component): popular page docs added to explain how query string is generated for building the popular games url fetch request

--- app/(pages)/popular/[slug]/page.tsx
+++ app/(pages)/popular/[slug]/page.tsx
@@ -1,60 +1,72 @@
-// import { apiKey, baseURL } from "@/app/lib/api";
-// import { fetchGamesFromAPI } from "@/app/lib/fetcher";
-// import { popularGameFilters, popularSlug } from "@/app/lib/api";
-// import GamesList from "@/app/sections/GamesList";
-
-// export async function generateStaticParams() {
-//   return Object.keys(popularGameFilters).map((slug) => ({ slug }));
-// }
-
-// export default async function Releases({
-//   params,
-// }: {
-//   params: { slug: popularSlug };
-// }) {
-//   const { slug } = await params;
-
-//   const query = popularGameFilters[slug];
-
-//   const url = `${baseURL}/games?${query}&key=${apiKey}`;
-
-//   const { results: games } = await fetchGamesFromAPI(url);
-
-//   return <GamesList games={games} />;
-// }
-
+// npm
 import {
   dehydrate,
   HydrationBoundary,
   QueryClient,
 } from "@tanstack/react-query";
 
-import { apiKey, baseURL, slugToQueryKey, URLSlug } from "@/app/lib/api";
+// libs
+import {
+  apiKey,
+  baseURL,
+  slugToQueryKey,
+  URLSlug,
+  getFilteredQueriesBySlug,
+} from "@/app/lib/api";
 import { fetchGamesFromAPI } from "@/app/lib/fetcher";
-import { getFilteredQueriesBySlug, queriesBySlug } from "@/app/lib/api";
+
+// components
 import GamesList from "@/app/sections/GamesList";
 
+/**
+ * Generates the static paths for the dynamic `popular/[slug]` route.
+ * Ensures that all slugs defined in `slugToQueryKey` are statically generated at build time.
+ */
 export async function generateStaticParams() {
   return Object.keys(slugToQueryKey).map((slug) => ({ slug }));
 }
+
+/**
+ * Renders a page of popular games based on a dynamic category (slug).
+ *
+ * Workflow:
+ * - Recieves a route slug ('best-of-the-year', 'popular-2024', 'all-stars')
+ * - Maps the slug to a query key (e.g., 'bestOfTheYear') using `slugToQueryKey`
+ * - Retrieves the actual query string from `getFilteredQueriesBySlug`
+ * - EXAMPLE - if slug is 'all-stars' - it is mapped to the key 'allStars' via `slugToQueryKey`
+ * - EXAMPLE - which then retrives query string `dates=2000-01-01,${currentDate}&ordering=-added&page_size=20`
+ * - Constructs the RAWG API URL with query string
+ * - Prefetches data on the server via React Query
+ * - Wraps the client component in <HydrationBoundary> to hydrate data on the client
+ */
 
 export default async function PopularGames({
   params,
 }: {
   params: { slug: URLSlug };
+  // slug is type defined with URLSlug because
+  // export type URLSlug = keyof typeof slugToQueryKey in lib/api.ts
+  // contains all valid url slugs expected in app
 }) {
+  // tanstack code
   const queryClient = new QueryClient();
 
+  // Destructured slug from route params (eg 'best-of-year')
+  // next js says that await is not needed - but next docs explain it is required because its async function - if removed console will flag error
   const { slug } = await params;
 
+  // Object literal map slug to query Key (eg 'lastMonth, nextMonth')
   const key = slugToQueryKey[slug];
 
+  // get the date filtered query string for returned key (eg dates=${string},${string}&ordering=-added&page_size=20)
   const query = getFilteredQueriesBySlug[key];
 
+  // construct api url string required to fetch dames
   const url = `${baseURL}/games?${query}&key=${apiKey}`;
 
+  // TANSTACK : Prefetch game data on the server to enable hydration on the client
   await queryClient.prefetchQuery({
-    queryKey: ["games", slug],
+    queryKey: ["games", slug], // unique key 'games' keep the cache isolated to this request
     queryFn: () => fetchGamesFromAPI(url),
   });
   return (
@@ -63,25 +75,3 @@ export default async function PopularGames({
     </HydrationBoundary>
   );
 }
-
-// export async function generateStaticParams() {
-//   return Object.keys(popularGameFilters).map((slug) => ({ slug }));
-// }
-
-// export async function fetchPopularGames(query: string) {
-//   return fetch(`${baseURL}/games?${query}&key=${apiKey}`).then((res) =>
-//     res.json()
-//   );
-// }
-
-// export default async function PopularGames({
-//   params,
-// }: {
-//   params: { slug: popularSlug };
-// }) {
-//   const { slug } = params;
-//   const query = popularGameFilters[slug];
-//   const games = await fetchPopularGames(query);
-
-//   return <GamesList initialData={games} />;
-// }
